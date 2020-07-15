7e075887d3b0fd0c051adcd58aa1665385877a4b
Huy Hoang <hoang.nhh1991@gmail.com>
2020-07-15 23:43:35 +0700

    fix count item left

--- src/selectors/TodoAppSelector.js
+++ src/selectors/TodoAppSelector.js
@@ -1,6 +1,5 @@
 export const countTodoLeft = state => {
-    return state.reduce((number, todo) =>{
-        console.log(number)
-        return number + !todo.isCompleted ? 1 : 0
-    },0)
+    return state.filter(todo => {
+        return !todo.isCompleted
+    }).length
 }
